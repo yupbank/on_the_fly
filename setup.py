f3ac3d749c47d6705519df33d6edf3ac76656b97
Peng Yu <peng.yu@shopify.com>
2016-12-21 23:38:13 -0500

    make pip install

---
+++ setup.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+
+from distutils.core import setup
+
+setup(
+        name = 'on_the_fly',
+        packages = ['on_the_fly',],
+        version = "0.01",
+        description = 'easy machine learning',
+        author = 'Peng Yu',
+        author_email = 'yupbank@gmail.com',
+        url = 'https://github.com/yupbank/on_the_fly',
+        download_url = 'https://github.com/yupbank/on_the_fly/archive/0.01.tar.gz',
+        keywords = ['on the fly', 'machine learning', 'sklearn', 'online'],
+        classifiers = [],
+        )
