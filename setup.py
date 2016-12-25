5def41ef15e69a8ae51d21859f6c2ed1c99fbe64
Peng Yu <peng.yu@shopify.com>
2016-12-25 17:07:49 -0500

    update version

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ from distutils.core import setup
 setup(
         name = 'on_the_fly',
         packages = ['on_the_fly',],
-        version = "0.01rc1",
+        version = "0.02rc1",
         description = 'easy machine learning',
         author = 'Peng Yu',
         author_email = 'yupbank@gmail.com',
         url = 'https://github.com/yupbank/on_the_fly',
-        download_url = 'https://github.com/yupbank/on_the_fly/archive/0.01rc.tar.gz',
+        download_url = 'https://github.com/yupbank/on_the_fly/archive/0.02rc.tar.gz',
         keywords = ['on the fly', 'machine learning', 'sklearn', 'online'],
         classifiers = [],
         )
